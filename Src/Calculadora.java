dc4af613c7c01edc90f2c429c32bbe2f50329b61
jpizarroc99 <jpizarroc99@gmail.com>
2025-06-13 20:38:43 -0400

    // Calculadora Javiera Pizarro / se activa el scanner para interactuar con el usuaria, luego se solicitan los numeros para las operaciones, luego de esto  se utilizan los sout en double para que sean más precisos en las operaciones. Se procede a realizar las operaciones con los numeros solicitados fijandonos que sean distintos de 0 para las operaciones de division y restos. por ultimo se muestran los resultados de cada operación /

---
+++ Src/Calculadora.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    //Solicitud de números
+        System.out.println("Ingresa un número: ");
+        double n1 = scanner.nextDouble();
+        System.out.println("Ingrese el segundo número: ");
+        double n2 = scanner.nextDouble();
+
+    //Opereciones
+        double suma = n1 + n2;
+        double resta = n1 - n2;
+        double multiplicacion = n1 * n2;
+        double division  =(n2 != 0) ? n1 /n2: 0;
+        double restoDiv =(n2 != 0) ? n1%n2: 0;
+
+    //Resultados
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicación; " + multiplicacion);
+    /**División**/
+    if (n1 != 0){
+        System.out.println("División; " + division);
+        System.out.println("Resto:"+ restoDiv);}
+    else {
+        System.out.println("No se puede dividir entre cero");}
+    scanner.close();
+    }
+}
